81b8505d6989827306ea7a029a54b708fd50e07e
KateIsabelle <kate.davis887@gmail.com>
2021-03-10 23:03:06 +0000

    fix wordsearch.js bug and add transpose function to have it read words vertically, update tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+    
+  const transposedLetters = transpose(letters);
+  const verticalJoin = transposedLetters.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
 
-module.exports = wordSearch
+  return false;
+};
+
+const transpose = function(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        result.push([matrix[i][j]]);
+      } else {
+        result[j].push(matrix[i][j]);
+      }
+    }
+  }
+  return result;
+};
+
+
+  
+module.exports = wordSearch;
